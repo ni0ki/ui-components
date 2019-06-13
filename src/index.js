c55824fae534fdacfd1a42ea588cce213270f817
Louati Mariem <mariem.louati@dashlane.com>
2019-06-13 16:34:19 +0200

    chore: add lint precommit hook

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+
+interface Props {
+  title: string;
+}
+
+const Button: FunctionComponent<Props> = ({
+  title = 'Nothing will happen!'
+}: Props): JSX.Element => (
+  <button
+    style={{
+      color: 'white',
+      backgroundColor: 'purple',
+      padding: '5px',
+      borderRadius: '5px',
+      cursor: 'pointer'
+    }}
+  >
+    {title}
+  </button>
+);
+
+export default Button;
