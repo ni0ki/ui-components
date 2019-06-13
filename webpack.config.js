d953b9fca9081c5ca2f765ebc7707d08598ac6b0
Louati Mariem <mariem.louati@dashlane.com>
2019-06-13 15:06:15 +0200

    chore: lint and format code with eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.tsx', '.ts']
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|build)/,
-                use: {
-                    loader: 'awesome-typescript-loader'
-                }
-            }
-        ]
-    },
-    externals: {
-        'react': 'react'
-    }
+  entry: './src/index.tsx',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'awesome-typescript-loader'
+        }
+      }
+    ]
+  },
+  externals: {
+    react: 'react'
+  },
+  plugins: [new CleanWebpackPlugin()]
 };
