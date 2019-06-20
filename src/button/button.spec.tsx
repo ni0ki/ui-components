a0c465ffcf456ca9c2aea7bf88e9958fcd385e5c
Louati Mariem <mariem.louati@dashlane.com>
2019-06-20 12:48:10 +0200

    chore: add styled components 💅 & adapted tests

--- src/button/button.spec.tsx
+++ src/button/button.spec.tsx
@@ -1,20 +1,43 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import 'jest-styled-components';
 import Button from '.';
+import { BasicButton } from '.';
 
 describe('<Button>', () => {
   it('should render with default props', () => {
-    const checkbox = shallow(<Button />);
+    const button = shallow(<Button />);
 
-    expect(checkbox.text()).toEqual('Nothing will happen!');
-    expect(checkbox).toMatchSnapshot();
+    expect(button.text()).toEqual('Nothing will happen!');
+    expect(button).toMatchSnapshot();
   });
 
   it('should render with provided title', () => {
     const title = 'whatever';
-    const checkbox = shallow(<Button title={title} />);
+    const button = shallow(<Button title={title} />);
 
-    expect(checkbox.text()).toEqual(title);
-    expect(checkbox).toMatchSnapshot();
+    expect(button.text()).toEqual(title);
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render with primary settings', () => {
+    const button = shallow(<Button primary={true} />);
+    expect(button).toMatchSnapshot();
+  });
+});
+
+describe('<BasicButton>', () => {
+  it('should render with default style', () => {
+    const button = mount(<BasicButton />);
+
+    expect(button).toHaveStyleRule('background-color', 'purple');
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render with primary style', () => {
+    const button = mount(<BasicButton primary={true} />);
+
+    expect(button).toHaveStyleRule('background-color', 'pink');
+    expect(button).toMatchSnapshot();
   });
 });
