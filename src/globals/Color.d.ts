6a7767001dcbdc1cbf14a006b2d86200e2ab6f94
Louati Mariem <mariem.louati@dashlane.com>
2019-10-09 16:36:46 +0200

    feat(sync): add new colors json for figma sync

--- src/globals/Color.d.ts
+++ src/globals/Color.d.ts
@@ -1,13 +1,15 @@
 export interface Color {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
+  '100': string;
+  '200': string;
+  '300': string;
+  '400': string;
+  '500': string;
+  '600': string;
+  '700': string;
+  '800': string;
+  '900': string;
 }
-export type ColorExclude<T extends number> = Omit<Color, T>;
-export type BasicColor = ColorExclude<100 | 200 | 400 | 600 | 800 | 900>;
+export type ColorExclude<T extends string> = Omit<Color, T>;
+export type BasicColor = ColorExclude<
+  '100' | '200' | '400' | '600' | '800' | '900'
+>;
--- src/globals/colors.ts
+++ src/globals/colors.ts
@@ -1,54 +1,18 @@
 import { BasicColor, ColorExclude } from '@globals/Color.d';
+import * as colors from '@globals/colors.json';
 
-export const $primary: ColorExclude<200 | 400> = {
-  100: '#cce5ea',
-  300: '#8cc4d0',
-  500: '#007c97',
-  600: '#005d71',
-  700: '#003945',
-  800: '#002e38',
-  900: '#062931'
-};
+export const $primary: ColorExclude<'200' | '400'> = colors.primary;
 
-export const $secondary: BasicColor = {
-  300: '#ffc1a3',
-  500: '#ff5412',
-  700: '#b34015'
-};
+export const $secondary: BasicColor = colors.secondary;
 
-export const $success: BasicColor = {
-  300: '#c1eac1',
-  500: '#20b422',
-  700: '#157917'
-};
+export const $success: BasicColor = colors.success;
 
-export const $danger: BasicColor = {
-  300: '#ffe3dd',
-  500: '#ff002d',
-  700: '#8b0018'
-};
+export const $danger: BasicColor = colors.danger;
 
-export const $warning: BasicColor = {
-  300: '#fffac1',
-  500: '#ffe900',
-  700: '#595200'
-};
+export const $warning: BasicColor = colors.warning;
 
-export const $info: BasicColor = {
-  300: '#00b8ff',
-  500: '#007dad',
-  700: '#00e042'
-};
+export const $info: BasicColor = colors.info;
 
-export const $light: ColorExclude<800> = {
-  100: '#ffffff',
-  200: '#fafafa',
-  300: '#f6f6f6',
-  400: '#e7e7e7',
-  500: '#d6d6d6',
-  600: '#9b9b9b',
-  700: '#474747',
-  900: '#191919'
-};
+export const $light: ColorExclude<'800'> = colors.light;
 
 export const $transparent = 'transparent';
