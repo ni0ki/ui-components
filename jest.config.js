c8b40a231693376d2c00d5fad84153ea6c366292
Louati Mariem <mariem.louati@dashlane.com>
2020-01-28 14:57:43 +0100

    chore: updating testing config

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: [
-    '<rootDir>/config/setupEnzyme.ts',
-    '<rootDir>/config/jsonTransform.js'
-  ],
+  setupFilesAfterEnv: ['<rootDir>/config/setupEnzyme.ts'],
   coverageReporters: ['text'],
   moduleNameMapper: {
     '^@typings/(.*)$': '<rootDir>/src/typings/$1',
@@ -16,7 +13,7 @@ module.exports = {
     '@atoms': '<rootDir>/src/atoms/index',
     '^@ions/(.*)$': '<rootDir>/src/ions/$1',
     '@ions': '<rootDir>/src/ions/index',
-    '@colors': '<rootDir>/src/colors/index',
+    '@colors': '<rootDir>/src/colors/colors.ts',
     '@colors/*': '<rootDir>/src/colors/*',
     '@icons/(.*)$': '<rootDir>/src/icons/icons-defs.svg'
   }
